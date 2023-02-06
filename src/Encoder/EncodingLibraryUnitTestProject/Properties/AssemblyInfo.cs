a298e0f4f147219e4f95aa54691909ef1968ed09
Meta <nikita-khokhlov@internet.ru>
2023-02-06 15:56:33 +0300

    Правка

---
+++ src/Encoder/EncodingLibraryUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EncodingLibraryUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EncodingLibraryUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d79bcbb7-813e-4089-9cc2-fb06fba98e46")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Encoder/EncodingLibraryUnitTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using EncodingLibrary.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EncodingLibraryUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UTF8_To_Windows1251()
+        {
+            var text = "Текст";
+            var expected = "РўРµРєСЃС‚";
+            var actual = text.ChangeEncoding("utf-8", "windows-1251");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
